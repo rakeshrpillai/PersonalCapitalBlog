8ddad79063ba6e01759c8753f872425cd3d50ece
Rakesh Pillai <rakesh.rk.p@gmail.com>
2018-11-11 23:12:15 -0800

    Initial commit

---
+++ app/src/main/java/com/rakeshpillai/personalcapitalblog/BlogListActivity.java
@@ -0,0 +1,12 @@
+package com.rakeshpillai.personalcapitalblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlogListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
